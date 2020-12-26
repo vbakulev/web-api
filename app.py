1e3992175b8342b66bd8d888050970c66c01ead5
vbakulev <70772962+vbakulev@users.noreply.github.com>
2020-12-26 10:55:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,9 @@ from flask import Flask, request, url_for, redirect, render_template, jsonify
 app = Flask(__name__)
 
 
-cols = ['Gender', 'Age']
-
-
 @app.route('/')
 def home():
     return render_template("home.html")
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    int_features = [x for x in request.form.values()]
-    prediction = '1'
-    prediction = int(prediction)
-    return render_template('home.html', pred='Expected Bill will be {}'.format(prediction))
 
 
 if __name__ == '__main__':
