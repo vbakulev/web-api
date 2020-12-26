1334df16cb893f3b174553647df7db563c417ec1
vbakulev <70772962+vbakulev@users.noreply.github.com>
2020-12-26 12:34:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import *
+from pycaret.classification import *
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
-
-cols = ['Gender', 'Age']
+model = load_model('model')
+cols = [
+    'Gender',
+    'Age',
+    'Neighbourhood',
+    'Scholarship',
+    'Hipertension',
+    'Diabetes',
+    'Alcoholism',
+    'Handcap'
+]
 
 
 @app.route('/')
@@ -13,10 +25,29 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [x for x in request.form.values()]
-    prediction = '1'
-    prediction = int(prediction)
-    return render_template('home.html', pred='Expected Bill will be {}'.format(prediction))
+    list_features = [x for x in request.form.values()]
+
+    if list_features[0] == 'Male':
+        list_features[0] == 'M'
+    elif list_features[0] == 'Famale':
+        list_features[0] == 'F'
+
+    for i in range(3, 8):
+        if list_features[i] == 'Yes':
+            list_features[i] = '1'
+        elif list_features[i] == 'No':
+            list_features[i] = '0'
+
+    final = np.array(list_features)
+    data_unseen = pd.DataFrame([final], columns=cols)
+    prediction = predict_model(model, data=data_unseen)
+
+    prediction = prediction.Label[0]
+    if prediction == 'Yes':
+        predict_text = 'Will not come!'
+    elif prediction == 'No':
+        predict_text = 'Will come!'
+    return render_template('home.html', pred=predict_text)
 
 
 if __name__ == '__main__':
